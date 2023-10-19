205dd25e24a8731faa55c25952a4ee8a6870d92e
zainal112714 <zainalarifindoraemon@gmail.com>
2023-10-19 07:00:27 +0700

    Modul 3 selesai

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useRef, useState } from "react";
+import { View, DrawerLayoutAndroid, StatusBar } from "react-native";
+import Header from "./components/header";
+import Button from "./components/button";
+import Separator from "./components/separator";
+import List from "./screens/list";
+import Article from "./screens/article";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+// Functional Component
+const App = () => {
+  // State Declaration
+  const [page, setPage] = useState("list");
+  // Ref Declaration
+  const drawer = useRef(null);
+
+  // Arrow Function inside Functional Component
+  const changePage = (drawer, pageName) => {
+    // Close Drawer
+    drawer.current.closeDrawer();
+    // Change state value
+
+    setPage(pageName);
+  };
+
+  // Arrow Function inside Functional Component
+  const navigationView = () => (
+    <View style={{ padding: 30, backgroundColor: "#222222", flex: 1 }}>
+      <Button text="List" onPress={() => changePage(drawer, "list")} />
+      <Separator height={30} />
+      <Button text="Article" onPress={() => changePage(drawer, "article")} />
+      <Separator height={30} />
+      <Button text="Close" onPress={() => drawer.current.closeDrawer()} />
     </View>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return (
+    <DrawerLayoutAndroid
+      ref={drawer}
+      drawerWidth={300}
+      drawerPosition="left"
+      renderNavigationView={navigationView}
+    >
+      <StatusBar style="dark" backgroundColor="#000" />
+      <View>
+        <Header drawer={drawer} />
+        {page == "list" ? <List /> : page == "article" ? <Article /> : null}
+      </View>
+    </DrawerLayoutAndroid>
+  );
+};
+
+export default App;
---
+++ screens/article.js
@@ -0,0 +1,189 @@
+import {
+  FlatList,
+  Image,
+  Text,
+  TouchableOpacity,
+  View,
+  StyleSheet,
+  Modal,
+  Pressable,
+  Alert,
+} from "react-native";
+
+import React, {useState} from 'react';
+
+// Dummmy Data (Array of Object)
+const datas = [
+  {
+    id: 1,
+    title:
+      "Dorong Pengembangan UMKM, IT Telkom Surabaya Gandeng UD Rozi dalam Pembuatan Website",
+    image:
+      "https://i.pinimg.com/originals/61/e7/3a/61e73a3f34859cae9c3de5df5d7157d7.jpg",
+  },
+  {
+    id: 2,
+    title: "Mahasiswa ITTelkom Surabaya Menang Kompetisi di New Delhi",
+    image:
+      "https://i.pinimg.com/originals/85/b9/ff/85b9ffaa5f065ea2ba832edce88e678f.png",
+  },
+  {
+    id: 3,
+    title:
+      "Benarkah Generasi Baru Generasi Strawberi? Ayo Belajar Growth Mindset dari Kampus ITTelkom Surabaya",
+    image:
+      "https://i.pinimg.com/originals/09/88/d4/0988d46ec7a40c30932b9cb51d0fac6d.jpg",
+  },
+  {
+    id: 4,
+    title: "4 Industri Tahan Resesi! Pastikan Kemampuanmu Termasuk Di Dalamnya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/mbacay-768x512.jpg",
+  },
+  {
+    id: 5,
+    title:
+      "Selamat Datang Mahasiswa Baru, PKKMB ITTelkom Surabaya Berlangsung Secara Luring",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/09/DSC_1931-768x512.jpg",
+  },
+  {
+    id: 6,
+    title:
+      "Dorong Pengembangan UMKM, IT Telkom Surabaya Gandeng UD Rozi dalam Pembuatan Website",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/Salinan-IMG_0823-768x512.jpg",
+  },
+  {
+    id: 7,
+    title: "Mahasiswa ITTelkom Surabaya Menang Kompetisi di New Delhi",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghjk-768x492.jpg",
+  },
+  {
+    id: 8,
+    title:
+      "Benarkah Generasi Baru Generasi Strawberi? Ayo Belajar Growth Mindset dari Kampus ITTelkom Surabaya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/asdfghkl-768x472.jpg",
+  },
+  {
+    id: 9,
+    title: "4 Industri Tahan Resesi! Pastikan Kemampuanmu Termasuk Di Dalamnya",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/10/mbacay-768x512.jpg",
+  },
+  {
+    id: 10,
+    title:
+      "Selamat Datang Mahasiswa Baru, PKKMB ITTelkom Surabaya Berlangsung Secara Luring",
+    image:
+      "https://ittelkom-sby.ac.id/wp-content/uploads/2022/09/DSC_1931-768x512.jpg",
+  },
+];
+
+// Functional Component
+const List = () => {
+  // Arrow Function with destructured argument
+  const [modalVisible, setModalVisible] = useState(false);
+  const renderItem = ({ item }) => {
+    return (
+      <View>
+           <Modal
+              animationType="slide"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+              Alert.alert('Modal has been closed.');
+              setModalVisible(!modalVisible);
+              }}>
+              <View style={styles.centeredView}>
+                  <View style={styles.modalView}>
+                      <Text style={styles.modalText}>Hello World!</Text>
+                      <TouchableOpacity
+                          style={[styles.button, styles.buttonClose]}
+                          onPress={() => setModalVisible(!modalVisible)}>
+                          <Text style={styles.textStyle}>Hide Modal</Text>
+                      </TouchableOpacity>
+                  </View>
+              </View>
+          </Modal>
+
+          <TouchableOpacity style={styles.view} onPress={() => setModalVisible(true)}>
+              <View>
+                  <Image source={{ uri: item.image }} style={styles.image} />
+                  <Text style={styles.text}>{item.title}</Text>
+               </View>
+          </TouchableOpacity>
+      </View>
+    );
+  };
+
+  return (
+    <FlatList
+      data={datas}
+      renderItem={renderItem}
+      keyExtractor={(item) => item.id}
+    />
+  );
+};
+
+// Styles
+const styles = StyleSheet.create({
+  view: {
+    padding: 15,
+    borderBottomColor: "#dddddd",
+    borderBottomWidth: 1,
+  },
+  image: {
+    height: 200,
+    width: null,
+  },
+  text: {
+    fontSize: 18,
+    paddingTop: 10,
+  },
+  centeredView: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginTop: 22,
+    },
+    modalView: {
+      margin: 20,
+      backgroundColor: 'white',
+      borderRadius: 20,
+      padding: 35,
+      alignItems: 'center',
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.25,
+      shadowRadius: 4,
+      elevation: 5,
+    },
+    button: {
+      borderRadius: 20,
+      padding: 10,
+      elevation: 2,
+    },
+    buttonOpen: {
+      backgroundColor: '#F194FF',
+    },
+    buttonClose: {
+      backgroundColor: '#2196F3',
+    },
+    textStyle: {
+      color: 'white',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    modalText: {
+      marginBottom: 15,
+      textAlign: 'center',
+    },
+});
+
+export default List;
